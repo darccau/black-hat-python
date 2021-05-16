3b9fb745db2fe13bde80e7de9f8fecbd9ffe82e2
duardopx <duardopx@protonmail.com>
2021-05-15 21:02:06 -0300

    fix: fix chapter name

--- chapter-2/udp_client.py
+++ chapter-2/udp_client.py
@@ -1,13 +1,13 @@
 import socket
 
-target_host = 127.0.0.1
-target_port  = 80
+target_host = '127.0.0.1'
+target_port  = 1337 
 
 # create socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
-client.sendto('PAYLOAD', (target_host, target_port))
+client.sendto(b'PAYLOAD', (target_host, target_port))
 
 # receive data
 data, addr = client.recvfrom(4096)
